d965732e1d67c84cab9fcab26007381f84f2cc53
Ahmed Hassan <you@example.com>
2023-07-10 12:54:21 +0300

    C basics lab 4

---
+++ C_Programming/C_Basics/Labs/Lab_4.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : Lab_4.c
+ Author      : Ahmed Hassan Ibrahim
+ Description : C Basics Lab 2 <<Calculate the summation of values between 1 and 99>>
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int i, sum = 0;
+
+	for(i = 1; i <= 99; i++)
+	{
+		sum += i;
+	}
+
+	printf("Summation from 1 to 99 = %d", sum);
+
+	return EXIT_SUCCESS;
+}
