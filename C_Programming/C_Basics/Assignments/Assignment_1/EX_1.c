23b7dc0054bbdc652a74e2391f6c553a2d40d1c8
Ahmed Hassan <you@example.com>
2023-07-10 14:13:21 +0300

    C basics assignment 1 EX 1

---
+++ C_Programming/C_Basics/Assignments/Assignment_1/EX_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : EX_1.c
+ Author      : Ahmed Hassan Ibrahim
+ Description : Assignment 1 EX 1 <<Write C Program to Print a Sentence>>
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("C Programming");
+
+	return EXIT_SUCCESS;
+}
