d26c2d68cfe1f65bc602d3a83f9a7e49eadb672d
Ahmed Hassan <you@example.com>
2023-07-25 17:12:50 +0300

    Array & Strings assignment EX 3.2

---
+++ C_Programming/Array_and_Strings/Assignments/EX_3.2.c
@@ -0,0 +1,32 @@
+/*
+ ============================================================================
+ Name        : EX_3.2.c
+ Author      : Ahmed Hassan Ibrahim
+ Description : Array & Strings EX 3.2 (Find string lenght)
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char str[100];
+	int i, length = 0;
+
+	printf("Enter a string: ");
+	fflush(stdin); fflush(stdout);
+	gets(str);
+
+	for(i = 0; i < 100; i++)
+	{
+		if(str[i] == 0)
+			break;
+		length++;
+	}
+
+	printf("\nLength of string = %d", length);
+
+	return EXIT_SUCCESS;
+}
