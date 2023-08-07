fb9359b47ade3bb930c1a8d2aca20ecc793a6cab
Ahmed Hassan <you@example.com>
2023-08-07 13:36:04 +0300

    Assignment 4 C Functions EX 2

---
+++ C_Programming/C_Functions/Assignments/EX_2.c
@@ -0,0 +1,43 @@
+/*
+ ============================================================================
+ Name        : EX_2.c
+ Author      : Ahmed Hassan Ibrahim
+ Description : Assignment 4 C functions EX 2 <Calculate factorial using recursion>
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fact(int x)
+{
+	int result = x;
+
+	if(x == 0 || x == 1)
+		return 1;
+
+	result *= fact(x-1);
+	return result;
+}
+
+int main(void) {
+
+	int input;
+
+	printf("Enter a positive integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d", &input);
+
+	while(input <= 0)
+	{
+		printf("Error!!! input is not a positive integer\n");
+		printf("Enter a positive integer: ");
+		fflush(stdin); fflush(stdout);
+		scanf("%d", &input);
+	}
+
+	printf("Factorial of %d = %d", input, fact(input));
+
+	return EXIT_SUCCESS;
+}
